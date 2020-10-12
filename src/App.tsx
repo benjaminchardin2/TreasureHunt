b23dc21d17a8b8f8946cc342b821d63485176cc0
benjaminchardin2 <38184160+benjaminchardin2@users.noreply.github.com>
2020-10-13 01:20:21 +0200

    style register

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Redirect, Route, Switch, withRouter,} from 'react-router-dom';
 import Register from "./components/register";
+import { faUser, faKey } from '@fortawesome/free-solid-svg-icons'
+import {library} from "@fortawesome/fontawesome-svg-core";
+
+library.add(faUser, faKey)
 
 function App() {
   return (
--- src/components/register.tsx
+++ src/components/register.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-import { Field, Form } from 'react-final-form';
+import {withRouter, RouteComponentProps} from 'react-router-dom';
+import {Field, Form} from 'react-final-form';
 import loginApi from "../network/loginApi";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import pirateShip from "../img/pirateShip.png"
 
-type PathParamsType = {
-};
+type PathParamsType = {};
 
 type Props = {
     history: any,
 };
 
-type State = {
-};
+type State = {};
 
 class Register extends React.Component<Props, State> {
-    state = {
-    };
+    state = {};
 
     onSubmit = (values) => {
-        const login = { username: values.login, password:  values.password , email: ''};
+        const login = {username: values.login, password: values.password, email: ''};
         loginApi.login(login)
             .then((response) => response.json())
             .then(json => {
@@ -33,39 +32,59 @@ class Register extends React.Component<Props, State> {
         return (
             <div className="page">
                 <div className="page-content">
-                    <Form
-                        onSubmit={this.onSubmit}
-                        render={(formRenderProps) => (
-                            <form onSubmit={formRenderProps.handleSubmit}>
-                                <div className="form-content">
-                                    <div className="form-group">
-                                        <label htmlFor="name-input" className="form-label">Nom d'équipe</label>
-                                        <Field
-                                            id="login-input"
-                                            name="login"
-                                            component="input"
-                                            className="form-field"
-                                            type="text"
-                                            required
-                                        />
-                                        <label htmlFor="name-input" className="form-label">Mdp (pas un vrai je les vois en clair après)</label>
-                                        <Field
-                                            id="password-input"
-                                            name="password"
-                                            component="input"
-                                            className="form-field"
-                                            type="text"
-                                            required
-                                        />
-                                    </div>
-                                    <button className="button primary" type="submit">C'est parti</button>
-                                </div>
-                            </form>
-                        )}
-                    />
+                    <div className="login-background">
+                        <div className="login-page">
+                            <div className="login-image">
+                                <a href='https://pngtree.com/so/pirate'><img className="login-picture"
+                                                                             src={pirateShip}/></a>
+                            </div>
+                            <div class="login-form">
+                                <Form
+                                    onSubmit={this.onSubmit}
+                                    render={(formRenderProps) => (
+                                        <form onSubmit={formRenderProps.handleSubmit}>
+                                            <div className="form-content">
+                                                <h1>S'inscrire</h1>
+                                                <div className="form-group">
+                                                    <label class="form-label" htmlFor="login-input">
+                                                        <FontAwesomeIcon icon={"user"}/>
+                                                    </label>
+                                                    <Field
+                                                        id="login-input"
+                                                        name="login"
+                                                        component="input"
+                                                        className="form-field-login"
+                                                        type="text"
+                                                        placeholder="login"
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="form-group">
+                                                    <label class="form-label" htmlFor="password-input">
+                                                        <FontAwesomeIcon icon={"key"}/>
+                                                    </label>
+                                                    <Field
+                                                        id="password-input"
+                                                        name="password"
+                                                        component="input"
+                                                        className="form-field-login"
+                                                        type="password"
+                                                        placeholder="password"
+                                                        required
+                                                    />
+                                                </div>
+                                                <button className="button primary" type="submit">C'est parti</button>
+                                            </div>
+                                        </form>
+                                    )}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
     }
 }
+
 export default withRouter(Register);
