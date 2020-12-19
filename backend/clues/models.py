97065ace6fde012548a38bf3cbe5daa99c5a835e
benjaminchardin2 <38184160+benjaminchardin2@users.noreply.github.com>
2020-12-19 10:28:36 +0100

    random code

--- backend/clues/models.py
+++ backend/clues/models.py
@@ -2,8 +2,13 @@ import uuid
 
 from django.db import models
 from django.contrib.auth import get_user_model
+import random
 
 User = get_user_model()
+
+
+def random_string():
+    return str(random.randint(1, 9999))
 
 
 class TreasureHunt(models.Model):
@@ -23,7 +28,7 @@ class Clues(models.Model):
 class TreasureHuntInstance(models.Model):
     pkid = models.BigAutoField(primary_key=True, editable=False)
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    treasureHunt = models.ForeignKey(TreasureHunt, to_field='id',  on_delete=models.CASCADE)
+    treasureHunt = models.ForeignKey(TreasureHunt, to_field='id', on_delete=models.CASCADE)
 
 
 class Participant(models.Model):
@@ -37,6 +42,6 @@ class Participant(models.Model):
 class AttributedClues(models.Model):
     pkid = models.BigAutoField(primary_key=True, editable=False)
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    code = models.IntegerField()
+    code = models.CharField(default=random_string, max_length=4)
     participant = models.ForeignKey(Participant, to_field='id', on_delete=models.CASCADE)
     clue = models.ForeignKey(Clues, to_field='id', on_delete=models.CASCADE)
