483315e67800913a86e7de89de014b6c064200ab
benjaminchardin2 <38184160+benjaminchardin2@users.noreply.github.com>
2020-12-19 10:18:16 +0100

    random code

--- backend/clues/serializers.py
+++ backend/clues/serializers.py
@@ -5,13 +5,13 @@ import logging
 
 
 class CluesSerializer(serializers.Serializer):
-    file = serializers.FileField(max_length=1000,
-                                 allow_empty_file=True, ),
     message = serializers.CharField(min_length=0)
+    id = serializers.UUIDField()
+    code = serializers.CharField(min_length=0)
 
     class Meta:
         model = CluesModel
-        fields = ('id', 'message')
+        fields = ('id', 'message', 'code')
 
 
 class TreasureHuntSerializer(serializers.Serializer):
@@ -23,8 +23,16 @@ class TreasureHuntSerializer(serializers.Serializer):
         fields = ('id', 'name')
 
 
+class CluesSerializerCreation(serializers.Serializer):
+    message = serializers.CharField(min_length=0)
+
+    class Meta:
+        model = CluesModel
+        fields = ('message')
+
+
 class TreasureHuntSerializerCustom(serializers.Serializer):
-    clues = CluesSerializer(many=True)
+    clues = CluesSerializerCreation(many=True)
     name = serializers.CharField(min_length=0)
 
     def create(self, validated_data):
